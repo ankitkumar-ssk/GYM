827ce1a57d83682911b40fea67e1559d9830b12f
ankitkumar-ssk <ankit.kumar@sureskills.com>
2025-04-15 14:31:23 +0530

    Dummy Commit

--- scr.js
+++ scr.js
@@ -3,7 +3,8 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
-    } else {
+    } 
+    else {
       x.className = "topnav";
     }
   }
